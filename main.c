2572e7376514ae5a21cae6e83460027b7b146b8d
tsm3 <tristan.slayde.mansfield@gmail.com>
2021-05-11 21:00:46 -0500

    fixed the WDT int

--- main.c
+++ main.c
@@ -32,10 +32,5 @@ void __attribute__ ((interrupt(WDT_VECTOR))) watchdog_timer (void)
 #error Compiler not supported!
 #endif
 {
-    if (!(flag_vec & PETF)) {
-        flag_vec |= TIMERF;
-    } else {
-        flag_vec &= ~PETF;
-    }
     __bic_SR_register_on_exit(LPM0_bits); // exit LPM3 when returning to program (clear LPM3 bits)
 }
